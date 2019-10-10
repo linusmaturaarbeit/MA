fe4b8257ddc54da732deb18ea5ee16fd7ec1d61e
linusmatura <54145183+linusmaturaarbeit@users.noreply.github.com>
2019-10-11 00:53:14 +0200

    Update QUANTIZE.py

--- QUANTIZE.py
+++ QUANTIZE.py
@@ -1,3 +1,21 @@
+# Software um Gespieltes zu transkribieren
+# Copyright (C) 2019  Linus Wesp
+#
+# This file is part of "Digitaler Kromarograph"
+#
+# "Digitaler Kromarograph" is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# "Digitaler Kromarograph" is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with "Digitaler Kromarograph".  If not, see <https://www.gnu.org/licenses/>.
+
 import math
 import itertools
 import ast
@@ -5,9 +23,6 @@ import math
 from operator import itemgetter
 
 givequantizedfilename ="quantized_midinotes.txt"
-
-# #statische Variable:
-# normal_tempo = 60
 
 def grid_intervall(metrum, quantize_note):
     metrum = int(float(metrum))
@@ -41,20 +56,10 @@ def seperate_time_and_hand(filename):
         list_only_time.append(i[1])
     return(list_only_time, list_only_hands)
 
-#variable Tempo
 def work(input_tempo, input_which_quantization, sourcefile, outputfile):
-    # print(sourcefile)
-
     #erster Viertel:
     grid_value = grid_intervall(input_tempo, input_which_quantization)
-
-
-    # -------------- Ab hier nur grid_value nötig --------------
-    # midiinputs_raw = read_listtxt_to_list(sourcefile) #format: [[0.00], [0.50], [1.00]]
-    #midiinputs = list(itertools.chain(*midiinputs_raw)) #flatting list : [0.00, 0.50, 1.00]
-
     midiinputs, only_hands = seperate_time_and_hand(sourcefile)
-
 
     grid_list = []
     last_item = midiinputs[-1]
@@ -63,15 +68,11 @@ def work(input_tempo, input_which_quantization, sourcefile, outputfile):
     last_item_rounded_up = math.ceil(last_item) #für grösse/länge des grids
 
     range_of_grid = int(last_item_rounded_up/grid_value)
-    #Bsp: 3viertel, auf 1/16 quan. --> 12er Raster
 
     # Liste für Raster
     for i in range(range_of_grid+1):
         grid_list.append(list_start_value)
         list_start_value += grid_value
-
-
-
 
     # -------------- Quantisierung --------------
     deltalist = []
@@ -80,21 +81,9 @@ def work(input_tempo, input_which_quantization, sourcefile, outputfile):
     for i in midiinputs:
         for j in grid_list:
             deltalist.append(abs(i-j))
-        #print(deltalist)
         quantized_index = deltalist.index(min(deltalist))
-        #print(quantized_index)
         quantized.append(grid_list[quantized_index])
         deltalist.clear()
-
-    #quantisierung wurde vorgenommen
-
-
-    #Jetzt die Listen zusammenbringen
-    #Ziel:   [[0,0],[1,0.5],[0,1],[1,1.5],[0,2]]        wären achtel rechts links, funf stück
-
-    # print(quantized)
-    # print("--------------------")
-    # print(only_hands)
 
     only_hands_and_time_quanized = []
     zwischenlist = []
@@ -103,15 +92,7 @@ def work(input_tempo, input_which_quantization, sourcefile, outputfile):
         zwischenlist = [f,b]
         only_hands_and_time_quanized.append(zwischenlist)
 
-
-
-    #
     # unflatten list and write it: [4, 5, 6] to [[4], [5], [6]]
     with open(outputfile, 'w') as f:
         for item in only_hands_and_time_quanized:
             f.write("%s\n" % item)
-
-# only_time, only_hands = seperate_time_and_hand("I_2019.08.14-16.47.16.txt")
-# print(only_time)
-# print("-------------------")
-# print(only_hands)
