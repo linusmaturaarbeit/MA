3abef83a2df5c491ea258ac97020bd204df57c28
linusmatura <54145183+linusmaturaarbeit@users.noreply.github.com>
2019-10-11 00:55:12 +0200

    Update executable.py

--- executable.py
+++ executable.py
@@ -1,5 +1,23 @@
+# Software um Gespieltes zu transkribieren
+# Copyright (C) 2019  Linus Wesp
+#
+# This file is part of "Digitaler Kromarograph"
+#
+# "Digitaler Kromarograph" is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# "Digitaler Kromarograph" is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with "Digitaler Kromarograph".  If not, see <https://www.gnu.org/licenses/>.
+
 #importiere die eigenen Module
-import WINDOWS_VERSION
+import MIDI_Instrument_Connection
 import QUANTIZE
 import TEXT_TO_MIDI
 #
@@ -27,9 +45,9 @@ midi_generated_filename_timestamped = return_filename(midi_generated_filename, "
 pdf_generated_filename = "PDF_"
 pdf_generated_filename_timestamped = return_filename(pdf_generated_filename, "pdf")
 
-#WINDOWS_VERSION
+#MIDI_Instrument_Connection
 try:
-    WINDOWS_VERSION.work(midi_input_filename_timestamped)
+    MIDI_Instrument_Connection.work(midi_input_filename_timestamped)
 except KeyboardInterrupt:
     print("quit recording\n")
 except NameError:
@@ -44,10 +62,10 @@ QUANTIZE.work(input_tempo, input_which_quantization,  midi_input_filename_timest
 
 #TEXT_TO_MIDI
 TEXT_TO_MIDI.work(midi_quantized_filename_timestamped, midi_generated_filename_timestamped, input_tempo)
-#print("\nMIDI-File generated under the name: '%s'" %(midi_generated_filename_timestamped))
-#
-#
+
 #MIDI_TO_PDF
+#Linux Opensuse:    'musescore -o "%s" "%s"' % (pdf_generated_filename_timestamped, midi_generated_filename_timestamped)
+#Windows 10    :    'MuseScore3.exe -o "%s" "%s"' % (pdf_generated_filename_timestamped, midi_generated_filename_timestamped)
 command = 'MuseScore3.exe -o "%s" "%s"' % (pdf_generated_filename_timestamped, midi_generated_filename_timestamped)
 subprocess.run(command)
 print("\nPDF-File generated under the name: '%s'" %(pdf_generated_filename_timestamped))
